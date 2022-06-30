0c3da01d153983449fd9d45984a0446c68e552cd
Daviddeivid2904 <lekermandavid@gmail.com>
2022-06-30 20:26:28 -0300

    creo que va bien, hay que ver que dice nacho

--- src/ingresoprofes/profes.js
+++ src/ingresoprofes/profes.js
@@ -94,94 +94,47 @@ const Dia = (props) => {
   );
 }
 
-function Board() {
-  const renderSquare = (dia) => {
-    return <Square dia = {dia}/>;
-  }
-
+function Board({ horario }) {
   const renderHora = (num,hora) => {
     return <Hora hora = {hora} num = {num}/>;
   }
 
-  const renderDia = (info) => {
-    return <Dia dia = {info}/>;
+const cuadradito = [];
+const cuadraDias = []
+
+  function bloques (){
+  for (var i = 1; i <= 5; i++) {
+    cuadradito.push(
+    <Square/>
+    )
+    }
   }
 
-  const status = "horarios"
+  function paraDias (){
 
-    return (
-      <div>
-        <h1 className="status">{status}</h1>
-        <div className="board-row">
-          {renderDia("Viernes")}
-          {renderDia("Jueves")}
-          {renderDia("Miercoles")}
-          {renderDia("Martes")}
-          {renderDia("Lunes")}
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare("setlunes1")}
-          {renderHora("1°","7:45 - 9:05")}
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderHora("2°","9:20 - 10:40")}
+     const dias = ["Viernes","jueves" , "Miercoles", "Martes","Lunes"]
 
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderHora("3°","10:55 - 12:15")}
+    for (var i = 0; i <= 4; i++) {
+      cuadraDias.push(
+      <Dia dia = {dias[i]}/>
+      )
+      }
+    }
 
-        </div>
-        <div className="board-row">
-          {renderDia()}
-          {renderDia()}
-          {renderDia()}
-          {renderDia()}
-          {renderDia()}
-          {renderDia()}
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderHora("4°","13:10 - 14:30")}
-
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderHora("5°","14:40 - 16:00")}
-
-        </div>
-        <div className="board-row">
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderSquare()}
-          {renderHora("6°","16:10 - 17:30")}
-
-        </div>
-      </div>
-    );
+  return(
+    <div>
+    <div className= "board-row">
+   {paraDias()}
+   {cuadraDias}
+    </div>
+    <div className= "board-row">
+   {bloques()}
+   {cuadradito}
+    <Hora hora = "1" num = "7:45 - 9:05"/>;
+    </div>
+    </div>
+  );
+ 
   }
 
 function Horarios (){
@@ -222,6 +175,71 @@ function Horarios (){
 //   };
 //   const [horario, setHorario] = useState(horarioVacio);
  
+//   const [name, setName] = useState("");
+//   const [apellido, setApellido] = useState("");
+//   const [materia, setMateria] = useState("");
+ 
+ 
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     alert(`The name you entered was: ${name} ${apellido} de ${materia}`)
+   
+//   }
+ 
+//   return (
+//     <div style = {formulario}>
+//     <form onSubmit={handleSubmit}>
+//       <label>ingrese su nombre:
+//         <input
+//           type="text"
+//           value={name}
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//       </label>
+//       <button className = "error">Enviar</button>
+//       <h1 style={bigblue}>{name.toLowerCase()}</h1>
+ 
+//       <label>ingrese apellido:
+//         <input
+//           type="text"
+//           value={apellido}
+//           onChange={(e) => setApellido(e.target.value)}
+//         />
+//       </label>
+//       <h1 style={bigblue}>{apellido.toLowerCase()}</h1>
+//       <label>ingrese materia:
+//         <input
+//           type="text"
+//           value={materia}
+//           onChange={(e) => setMateria(e.target.value)}
+//         />
+//       </label>
+//       <h1 style={bigblue}>{materia.toLowerCase()}</h1>
+//       <Horarios horario={horario}/>
+//     </form>
+//     </div>
+//   )
+// }
+ 
+// const Square = (props) => {
+ 
+//   const [tocado, setToca] = useState(false);
+ 
+//   function Tocar (){
+//    tocado? setToca(false): setToca(true)
+//   }
+ 
+//     //console.log("lunes 1 esta " + lunes_1)
+//     //props.dia(true)
+//     //console.log("lunes 1 esta " + lunes_1)
+//     return (
+//     <div className="square" onClick={Tocar}>
+//       <div className = "click">
+//         {tocado ? "✅" : ""}
+//       </div>
+//     </div>
+//   );
+// }
 // const Hora = (props) => {
 //   return (
 //     <div className="izq">
@@ -277,6 +295,3 @@ function Horarios (){
 //       </div>
 //     );
 //   }
-
-
-
