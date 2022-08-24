b5bc4b2b8f137d6d874438381318f508e45d0c55
Daviddeivid2904 <lekermandavid@gmail.com>
2022-08-24 11:38:41 -0300

    materias ty profesores listados

--- src/ingresomaterias/materias.js
+++ src/ingresomaterias/materias.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useState } from "react";
 import {Materia} from "./newMaterias";
 import "./styleMaterias.css";
@@ -7,15 +7,31 @@ export {ListaMaterias}
 function ListaMaterias() {
 
 const [show,setShow] = useState(false);
+const [users, setUsers] = useState();
+
+const getApiData = async () => {
+  const response = await fetch(
+    "http://localhost:5000/materias"
+  ).then((response) => response.json());
+  console.log(response);
+  setUsers(response);
+};
+
+useEffect(() => {
+  getApiData();
+}, []);
 
     return (
-<div>
-    <div className='listamaterias'>
-
-    </div>
-    <div className = "newMateria">
-        <button className = "botonMateria" onClick={()=> setShow(true)}>Nueva materia</button>
-    </div>
+<div className="lista">
     <Materia onClose = {()=> setShow(false)} show = {show}/>
+    <div>
+        <ul className='uli'>
+           {users && users.map((user) => (
+           <li className='liliana'><div className = "nombres">{user.nombre_materia}</div></li>      
+          
+             ))}
+        </ul>
+    </div>
+        <button className = "botonMateria" onClick={()=> setShow(true)}>Nueva materia</button>
 </div>
     )}
