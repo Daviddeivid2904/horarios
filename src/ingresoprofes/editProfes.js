369fe725ae3964f5ca3dc463f80896d03a3c3ea0
Daviddeivid2904 <lekermandavid@gmail.com>
2022-09-28 20:30:19 -0300

    se puede editar todo pero no mostrar en value

---
+++ src/ingresoprofes/editProfes.js
@@ -0,0 +1,263 @@
+import { useState } from "react";
+import React, {useEffect} from 'react';
+import "./styleProfes.css"
+
+const Editar = props => {
+
+  const [nombre, setName] = useState("");
+
+  const [apellido, setApellido] = useState("");
+
+  const [materia, setMateria] = useState("");
+
+  const [idMateria, setIdMateria] = useState("");
+
+  const [posibles_materias, setPosibles] = useState();
+
+  const [users, setUsers] = useState([{nombre_materia:"none",id_materia:0}],[]);
+
+  const horarioVacio = [
+    [false, false, false, false, false, false,"lunes"],
+    [false, false, false, false, false, false,"martes"],
+    [false, false, false, false, false, false,"miercoles"],
+    [false, false, false, false, false, false,"jueves"],
+    [false, false, false, false, false,"viernes"],
+  ];
+
+  const [disponibilidad, setDisponibilidad] = useState(horarioVacio)
+
+  const fetchData = async() => {
+    fetch( "http://localhost:5000/profesores/"+props.id_profesor)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        setUsers(data)
+        console.log(data)
+      })
+  }
+
+  const setaerMateria = (e) =>{
+    getApiData();
+    setMateria(e.target.value)
+    setIdMateria(e.target.value.id_materia)
+    console.log("materia es " + materia + "su ID es" + idMateria)
+  }
+
+  const getApiData = async () => {
+    const response = await fetch(
+      "http://localhost:5000/materias"
+    ).then((response) => response.json());
+    console.log(response);
+    setPosibles(response);
+  };
+
+  useEffect(() => {
+    getApiData();
+    fetchData();
+    console.log(props);
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    alert(`The name you entered was: ${nombre} ${apellido} de ${materia}`)
+
+    fetch("http://localhost:5000/editarDatos/"+props.id_profesor, {
+      method: 'PUT',
+      body: JSON.stringify({
+        nombre: nombre,
+        apellido: apellido
+      }),
+      headers: {
+        'Content-Type': 'application/json'// AQUI indicamos el formato
+      }
+    })
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(data) {
+      console.log(data);
+    })
+    .catch(function(error) {
+      console.error(error);
+    })
+
+  }
+
+  if(!props.show){
+    return null
+  }
+
+  return (
+    <div className = "shadow">
+      <div className = "formulario">
+        <form onSubmit={handleSubmit}>
+          <div className = "formHeader">
+            <p onClick = {props.onClose} className = "boton">X</p>
+          </div>
+          <label>Nombre del docente:
+            <br></br>
+            <input 
+              className = "inputs"
+              placeholder = "Escribe aquí"
+              type="text" 
+              defaultvalue={nombre}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+            <br></br>
+            <label>Apellido del docente:
+            <br></br>
+            <input 
+              className = "inputs"
+              placeholder = "Escribe aquí"
+              type="text" 
+              defaultvalue={apellido}
+              onChange={(e) => setApellido(e.target.value)}
+            />
+          </label>
+          <br></br>
+          <label> Materia que da:
+            <br></br>
+            <select name="materias" onChange={setaerMateria}>
+              {posibles_materias && posibles_materias.map((user) => (
+                  <option value = {user.nombre_materia}>{user.nombre_materia}</option>
+              ))}
+            </select>
+          </label>
+        <Horarios setDisponibilidad = {setDisponibilidad} disponibilidad = {disponibilidad}/>
+        <button className = "boton">Guardar</button>
+       </form>
+    </div>
+  </div>
+  )
+}
+
+
+
+const Square = (props) => {
+
+  const [tocado, setToca] = useState(false);
+  const newHorario = [...props.disponibilidad];
+  function Tocar (){
+       tocado? setToca(false) : setToca(true)
+
+       if(tocado) newHorario[props.Fila][props.Columna] = false 
+       else newHorario[props.Columna][props.Fila] = true
+       props.setDisponibilidad(newHorario)
+      }
+        return (
+        <div className="square" onClick={Tocar}>
+          <div className = "click">
+            {tocado ? "✅" : ""}
+          </div>
+        </div>
+      );
+    }
+const Hora = (props) => {
+  return (
+    <div className="izq">
+      <p className="numero">{props.num}</p>
+      <div className = "hora">
+          {props.hora}
+      </div>
+    </div>
+  );
+}
+const Dia = (props) => {
+  return (
+    <div className="dia">
+      {props.dia}
+    </div>
+  );
+}
+
+const Board = (props) => {
+    const cuadraDias = [];
+    const vacio = []
+
+
+  function paraDias (){
+
+     const dias = ["Viernes", "jueves" , "Miercoles", "Martes","Lunes"]
+
+    for (var i = 0; i <= 4; i++) {
+      cuadraDias.push(
+      <Dia dia = {dias[i]}/>
+      )
+      }
+
+      return (
+        <div className= "board-row">
+        {cuadraDias}
+        </div>
+      )
+    }
+  function diasVacios (){
+
+      const dias = ["","", "","","",""]
+ 
+     for (var i = 0; i <= 5; i++) {
+       vacio.push(
+       <Dia dia = {dias[i]}/>
+       )
+       }
+
+      return(vacio)
+     }
+
+     
+  function tabla1(){
+       const horas = ["7:45 - 9:05","9:20 - 10:40", "10:55 - 12:15"] 
+       const cant_veces = [4,3,2,1,0]
+    return(  
+      horas.map(numero => 
+        <div className= "board-row"> 
+          {cant_veces.map(i => <Square setDisponibilidad = {props.setDisponibilidad} disponibilidad = {props.disponibilidad} Columna = {i} Fila = {horas.indexOf(numero)}/>)}
+          <Hora hora = {numero} num = {horas.indexOf(numero) + 1}/>
+        </div> 
+        )
+    )
+  }
+
+  function tabla2(){
+    const horas = ["13:10 - 14:30","14:40 - 16:00", "16:10 - 17:30"] 
+    const cant_veces = [4,3,2,1,0]
+ return(  
+   horas.map(numero => 
+     <div className= "board-row"> 
+       {cant_veces.map(i => <Square setDisponibilidad = {props.setDisponibilidad} disponibilidad = {props.disponibilidad} Columna = {i} Fila = {horas.indexOf(numero) + 3}/>)}
+       <Hora hora = {numero} num = {horas.indexOf(numero) + 4}/>
+     </div> 
+     )
+ )
+}
+  
+  return(
+    <div>
+      <h1 className="status">Horarios disponibles: </h1>
+      <br></br>
+    {paraDias()}
+    {tabla1()}
+    {diasVacios()}
+    {tabla2()}
+    </div>
+  );
+ 
+  }
+
+  const Horarios = (props) => {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board setDisponibilidad = {props.setDisponibilidad} disponibilidad = {props.disponibilidad} />
+        </div>
+        <div className="game-info">
+        </div>
+      </div>
+    );
+  }
+
+  export {Editar}
+
--- src/ingresoprofes/profesores.js
+++ src/ingresoprofes/profesores.js
@@ -1,35 +1,55 @@
 import React, {useEffect} from 'react';
 import { useState } from "react";
 import {Inputs} from "./nuevoProfes";
+import {Editar} from "./editProfes";
 import "./styleProfes.css";
 export {ListaProfes}
 
 function ListaProfes() {
 
 const [show,setShow] = useState(false);
+const [showEditar,setShowEditar] = useState(false);
 const [users, setUsers] = useState();
+const [id,setId] = useState(0);
 
 const getApiData = async () => {
   const response = await fetch(
     "http://localhost:5000/profesores"
   ).then((response) => response.json());
-  console.log(response);
+  //console.log(response);
   setUsers(response);
 };
+
+function abrir(value) {
+  setId(value)
+  setShowEditar(true);
+  }
 
 useEffect(() => {
   getApiData();
 }, []);
 
 
+/*const eliminar = (event) => {
+    fetch('http://localhost:5000//deleteProfe/'+event, {
+      method: 'DELETE',
+    })
+    .then(res => {
+      return res.json()
+    }) 
+    .then(data => console.log(data))
+}*/
+
+
     return (
 <div className="lista">
     <Inputs onClose = {()=> setShow(false)} refresh = {getApiData()} show = {show}/>
+    <Editar onClose = {()=> setShowEditar(false)} refresh = {getApiData()} show = {showEditar} id_profesor = {id}/>
     <div>
         <ul className='uli'>
            {users && users.map((user) => (
              <div>
-                   <li className='liliana'><div className = "nombres">{user.nombre +" "+ user.apellido}</div></li> 
+                   <li className='liliana'><div onClick={()=>abrir(user.id_profesor)} className = "nombres">{user.nombre +" "+ user.apellido}</div><div /*onClick = {eliminar(user.id_profesor)}*/>X</div></li> 
              </div>
            ))}
         </ul>
